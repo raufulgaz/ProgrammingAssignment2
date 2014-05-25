4af83354f3e2933d036fc31209e4b40f0c9b97c2
raufulgaz <raufulgaz@gmail.com>
2014-05-25 09:14:33 +0200

    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming - Assignment 2
+## this R script contains two functions
+## "makeCacheMatrix" that creates a cacheable matrix 
+## "cacheSolve" that inverts a matrix
 
-## TEST COMMENT
-
-## Write a short comment describing this function
+## makeCacheMatrix function
+## 'x' is the matrix to be cached.
+## return a list of four functions:
+## - set: overwrite the matrix
+## - get: returns the values of the matrix
+## - setinv: input to the inverse of the matrix
+## - getinv: returns the inverted matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ##cleans variable
+  inv <- NULL
+  # set the matrix; remove cache
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  ## get the value of the matrix
+  get <- function() x
+  # set and get the inverse
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  # return list of all the functions
+  list(set = set,
+       get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function
+## inverts a matrix. 
+## if a cached solution is found, it is returned; 
+## otherwise, an inverse is calculated, cached, and returned.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # if cached inverse exists, return it
+  if(!is.null(x$getinv())){
+    message("returning cached solution.")
+    x$getinv()
+  }
+  # otherwise, solve and cache new inverse
+  else {
+    message("cached solution not found ... new solution...")
+    x$setinv(solve(x$get()))
+    message("returning cached solution.")
+    x$getinv()
+  } 
+  ## Return a matrix that is the inverse of 'x'
 }
